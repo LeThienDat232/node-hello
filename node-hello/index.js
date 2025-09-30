72fd1c4db9881b5eec09db9fb31401275dd82515
LeThienDat232 <lethiendat2005@gmail.com>
2025-09-30 13:39:27 +0700

    Fix package.json and index.js for Render

--- node-hello/index.js
+++ node-hello/index.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
 
